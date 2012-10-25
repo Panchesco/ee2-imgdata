b0b3846b68cd80e39edad5e6422cd8d8dc5244ac
Panchesco <panchesco@panchesco.com>
2012-10-25 11:52:27 -0700

    resolving merge conflict

--- pi.imgdata.php
+++ pi.imgdata.php
@@ -29,14 +29,7 @@
 			var $tag;
 			var $alt;
 			var $html;
-<<<<<<< HEAD
 			var $data =	array('width' => '','height' => '','type' => '','attr_str' => '');
-=======
-			var $data =	array(	'width'=>'',
-								'height'=>'',
-								'type'=>'',
-								'attr_str'=>'');
->>>>>>> 186a1de002fe4b30ef8062bd3f663cd46a71b7ac
 			var $filename;
 		
 		
@@ -150,15 +143,11 @@
 
 
 
-<<<<<<< HEAD
-				// ----------------------------------------  
-				// Usage  
-				// ----------------------------------------  
-=======
+
 		// ----------------------------------------  
 		// Usage  
 		// ----------------------------------------  
->>>>>>> 186a1de002fe4b30ef8062bd3f663cd46a71b7ac
+
 
                   
                 function usage()  
@@ -166,22 +155,11 @@
                 ob_start();                
                 ?>
                 
-                                
-<<<<<<< HEAD
-                Simple plugin to make PHP getimagesize() function data available as formatted html tag or strings.
-                In template where you're outputting an image, insert following instead of html img tag.
-                
-                
-                ------------------
-				TAGS:
-				------------------
-=======
-                Simple ExpressionEngine 2 plugin to make PHP getimagesize function data available as formatted html img tag or strings.
+
                                 
 		------------------
 		TAGS:
 		------------------
->>>>>>> 186a1de002fe4b30ef8062bd3f663cd46a71b7ac
                 
                 ### {exp:imgdata:tag url="http://url/of/image.jpg" alt="Alt String" html="any additional html you want added to the img tag"}
                 Returns HTML img tag with width, height, alt and any custom html
@@ -201,7 +179,7 @@
                 Returns image width and height attributes for img tag
                 Example: width="100" height="75"
                 
-<<<<<<< HEAD
+
                 
                 
 		        ------------------
@@ -216,20 +194,7 @@
 				
 				###	html='class="thumb"'
 				Any custom html, such as css or title attribute to add to img tag
-=======
-		------------------
-		PARAMETERS:
-		------------------
-		
-		###	url="http://url/to/my_image.jpg"
-		URL of img file
-				
-		###	alt="Alt Text"
-		Alt text to add to img tag
-				
-		###	html='class="thumb"'
-		Any custom html, such as css or title attribute to add to img tag
->>>>>>> 186a1de002fe4b30ef8062bd3f663cd46a71b7ac
+
 
                 
                 <?php
@@ -243,7 +208,7 @@
                 
 		}
 		
-		/* ******* */
+
 
 /* End of file pi.imgdata.php */  
 /* Location: ./system/expressionengine/third_party/imgdata/pi.imgdata.php */  
